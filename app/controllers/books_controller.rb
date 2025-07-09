3e2ae5640b0b470507be19013b08135b703a3d75
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-09 07:43:16 +0000

    [Finish] RSpec_test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,21 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -32,15 +37,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @user = Book.find(params[:id]).user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 end
