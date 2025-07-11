d046b77630b8082354a7666dd782f4089b3744d4
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-11 13:15:09 +0000

    [Fix] no word search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,23 +1,26 @@
 class SearchesController < ApplicationController
   def search
-    if params[:table] == User.name
-      @searches = User.all
-      search_cell = :name
-    elsif params[:table] == Book.name
-      @searches = Book.all
-      search_cell = :title
-    else
+    if params[:word].empty?
       @searches = []
-    end
 
-    if params[:how] == "exact_match"
-      @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]))
-    elsif params[:how] == "forward_match"
-      @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]) + "%")
-    elsif params[:how] == "backward_match"
-      @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]))
-    elsif params[:how] == "partial_match"
-      @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]) + "%")
+    else
+      if params[:table] == User.name
+        @searches = User.all
+        search_cell = :name
+      elsif params[:table] == Book.name
+        @searches = Book.all
+        search_cell = :title
+      end
+
+      if params[:how] == "exact_match"
+        @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]))
+      elsif params[:how] == "forward_match"
+        @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]) + "%")
+      elsif params[:how] == "backward_match"
+        @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]))
+      elsif params[:how] == "partial_match"
+        @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]) + "%")
+      end
     end
   end
 end
