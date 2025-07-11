ffead633971a26e999b399d9081de9375fffb723
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-11 12:40:13 +0000

    [Finish] search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,23 @@
 class SearchesController < ApplicationController
   def search
-    if params[:table] == 'User'
-      @table = User.where("name = ?", params[:word])
-    elsif params[:table] == 'Book'
-      @table = Book.where("title = ?", params[:word])
+    if params[:table] == User.name
+      @searches = User.all
+      search_cell = :name
+    elsif params[:table] == Book.name
+      @searches = Book.all
+      search_cell = :title
+    else
+      @searches = []
+    end
+
+    if params[:how] == "exact_match"
+      @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]))
+    elsif params[:how] == "forward_match"
+      @searches = @searches.where("#{search_cell} LIKE?", @searches.sanitize_sql_like(params[:word]) + "%")
+    elsif params[:how] == "backward_match"
+      @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]))
+    elsif params[:how] == "partial_match"
+      @searches = @searches.where("#{search_cell} LIKE?", "%" + @searches.sanitize_sql_like(params[:word]) + "%")
     end
   end
 end
