99b737606c9d5c232a6d2a8df5da49f582fac1f9
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-12 07:24:26 +0000

    [Finish] asynchronous

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private
