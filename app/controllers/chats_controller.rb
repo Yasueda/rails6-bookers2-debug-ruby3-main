76213ccecc39b8215da03c37a01dab46bd2c2076
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-13 13:57:04 +0000

    [Finish] ChatController

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,44 @@
 class ChatsController < ApplicationController
+  before_action :mutual_follow, only: [:show]
+
+  def show
+    # 相手ユーザーを取得
+    @partner = User.find(params[:id])
+
+    # current_idが登録されているroom_idのリスト
+    current_rooms = current_user.user_rooms.pluck(:room_id)
+
+    # 相手ユーザーがcurrent_idが参加しているroom_idの中にいるか
+    user_room = UserRoom.find_by(user_id: @partner.id, room_id: current_rooms)
+
+    unless user_room.nil?
+      @room = user_room.room
+    else
+      @room = Room.new
+      @room.save
+
+      UserRoom.create(user_id: current_user.id, room_id: @room.id)
+      UserRoom.create(user_id: @partner.id, room_id: @room.id)
+    end
+
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room_id)
+  end
+
+  def create
+    @chat = current_user.chats.new(chat_params)
+  end
+
+  def destroy
+    @chat = current_user.chats.find(params[:id])
+    @chat.destroy
+  end
+
+  def mutual_follow
+    partner = User.find(params[:id])
+
+    unless current_user.followed_by?(partner) && partner.followed_by?(current_user)
+      redirect_to :back
+    end
+  end
 end
--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get show" do
+    get chats_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get chats_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get chats_destroy_url
+    assert_response :success
+  end
 end
