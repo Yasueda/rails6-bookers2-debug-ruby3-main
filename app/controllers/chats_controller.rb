d0bcf15c2f7f15261fca03e934dba00a230d393f
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-14 03:16:27 +0000

    [Start] chat_room

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -11,35 +11,43 @@ class ChatsController < ApplicationController
     # 相手ユーザーがcurrent_idが参加しているroom_idの中にいるか
     user_room = UserRoom.find_by(user_id: @partner.id, room_id: current_rooms)
 
+    # roomが無ければ作る
     unless user_room.nil?
       @room = user_room.room
     else
       @room = Room.new
       @room.save
 
+      # UserRoomに情報を記録する
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @partner.id, room_id: @room.id)
     end
 
+    # roomのチャットデータを取得
     @chats = @room.chats
     @chat = Chat.new(room_id: @room_id)
   end
 
   def create
     @chat = current_user.chats.new(chat_params)
+    @chat.save
+    @chats = Room.find(chat_params[:room_id]).chats
+    redirect_to request.referer
   end
 
   def destroy
     @chat = current_user.chats.find(params[:id])
     @chat.destroy
+    redirect_to request.referer
   end
 
   private
 
   def chat_params
-    params.require(:chat).permit(:massege, :room_id)
+    params.require(:chat).permit(:message, :room_id)
   end
 
+  # 相互フォローしているかどうか
   def mutual_follow
     partner = User.find(params[:id])
 
